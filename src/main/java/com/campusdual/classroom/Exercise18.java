2f297df4b697ec3f0a6e1ba8c66e023c6492f17d
Gabriel <gabriel.lamela.ch@gmail.com>
2024-11-22 14:17:44 +0100

    Ejercicio 18 resuelto

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,34 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
+import java.util.Arrays;
+
 public class Exercise18 {
 
+	public static int[] createAndInitializeArray(int length){
+		int[] intArray = new int[length];
+		for (int i = 1; i <= length; i++) {
+			intArray[i - 1] = i;
+		}
+		return intArray;
+	}
+
+	public static void showInlineArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (i < arr.length - 1) {
+				System.out.print(arr[i] + " ");
+			} else {
+				System.out.print(arr[i]);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+
+		int numInput = Utils.integer("Introduce un número entero: ");
+		int[] array = createAndInitializeArray(numInput);
+		showInlineArray(array);
 
 	}
 }
